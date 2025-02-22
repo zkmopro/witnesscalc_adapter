ba288c3423da7a0884ffe32e603a54e2411ebaf3
erhant <erhany96@gmail.com>
2025-02-22 10:25:24 +0300

    address comments

--- witnesscalc_adapter/src/convert_type.rs
+++ witnesscalc_adapter/src/convert_type.rs
@@ -5,88 +5,99 @@ use num_traits::FromBytes;
 
 pub fn parse_witness_to<T>(
   buffer: &[u8],
-  map_chunk: impl Fn(&[u8]) -> T,
+  from_le_bytes: impl Fn(&[u8]) -> T,
 ) -> io::Result<Vec<T>> {
-  let mut pos = 0;
+    /// Prints to the standard ouput only in debug build.
+    macro_rules! debug_println {
+        ($($arg:tt)*) => (#[cfg(debug_assertions)] println!($($arg)*));
+    }
 
-  // skip format bytes (4 bytes)
-  // this says "wtns" in ASCII
-  pos += 4;
+    let mut pos = 0;
 
-  // read version (4 bytes)
-  let _version = u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
-  // println!("version: {:?}", _version);
-  pos += 4;
+    // skip format bytes (4 bytes)
+    // ensure that this says "wtns" in ASCII  
+    if &buffer[pos..pos + 4] != b"wtns" {  
+      return Err(io::Error::new(  
+          io::ErrorKind::InvalidData,  
+          "Invalid witness file format.",  
+      ));  
+    }  
+    pos += 4;
 
-  // read number of sections (4 bytes)
-  let n_sections = u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
-  // println!("n_sections: {:?}", n_sections);
-  pos += 4;
+    // read version (4 bytes)
+    let _version = u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
+    debug_println!("version: {}", _version);
+    pos += 4;
 
-  // number of 8 bit integers per field element
-  let mut n8 = 0;
+    // read number of sections (4 bytes)
+    let n_sections = u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
+    debug_println!("n_sections: {}", n_sections);
+    pos += 4;
 
-  // iterate through sections to find section_id = 2 (witness data)
-  //
-  // each [section]
-  // - section id (4 bytes)
-  // - section length (8 bytes)
-  for _ in 0..n_sections {
-      let section_id = u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
-      println!("section_id: {:?}", section_id);
-      pos += 4;
+    // number of 8 bit integers per field element
+    let mut n8 = 0;
 
-      let section_length = u64::from_le_bytes(buffer[pos..pos + 8].try_into().unwrap());
-      println!("section_length: {:?}", section_length);
-      pos += 8;
+    // iterate through sections to find section_id = 2 (witness data)
+    //
+    // each [section]
+    // - section id (4 bytes)
+    // - section length (8 bytes)
+    for _ in 0..n_sections {
+        let section_id = u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
+        debug_println!("section_id: {}", section_id);
+        pos += 4;
 
-      match section_id {
-          // [section 1]
-          // - `n8` number of 8 bit integers per field element (4 bytes / u32)
-          // - the field `q` value (32 bytes)
-          // - number of witness values (4 bytes / `u32`)
-          1 => {
-              n8 = u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
-              println!("n8: {:?}", n8);
-              pos += 4;
+        let section_length = u64::from_le_bytes(buffer[pos..pos + 8].try_into().unwrap());
+        debug_println!("section_length: {}", section_length);
+        pos += 8;
 
-              let _q = BigInt::from_signed_bytes_le(&buffer[pos..pos + 32]);
-              println!("q: {:?}", _q);
-              pos += 32;
+        match section_id {
+            // [section 1]
+            // - `n8` number of 8 bit integers per field element (4 bytes / u32)
+            // - the field `q` value (32 bytes)
+            // - number of witness values (4 bytes / `u32`)
+            1 => {
+                n8 = u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
+                debug_println!("n8: {}", n8);
+                pos += 4;
 
-              let _n_witness_values =
-                  u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
-              println!("n_witness_values: {:?}", _n_witness_values);
-              pos += 4;
-          }
+                let _q = BigInt::from_signed_bytes_le(&buffer[pos..pos + 32]);
+                debug_println!("q: {}", _q);
+                pos += 32;
 
-          // [section 2]
-          // - witness data (`n8` bytes per element, section_length bytes total)
-          2 => {
-              // read & convert witness bytes chunk by chunk
-              let elements: Vec<T> = buffer[pos..pos + section_length as usize]
-                  .chunks(usize::try_from(n8).unwrap())
-                  .map(map_chunk)
-                  .collect();
+                let _n_witness_values =
+                    u32::from_le_bytes(buffer[pos..pos + 4].try_into().unwrap());
+                    debug_println!("n_witness_values: {}", _n_witness_values);
+                pos += 4;
+            }
 
-              return Ok(elements);
-          }
-          // skip any other section
-          _ => {
-              pos = pos + section_length as usize;
-          }
-      }
-  }
+            // [section 2]
+            // - witness data (`n8` bytes per element, `section_length` bytes total)
+            2 => {
+                // read & convert witness bytes chunk by chunk
+                let elements: Vec<T> = buffer[pos..pos + section_length as usize]
+                    .chunks(usize::try_from(n8).unwrap())
+                    .map(from_le_bytes)
+                    .collect();
 
-  Err(io::Error::new(
-      io::ErrorKind::InvalidData,
-      "Witness section not found.",
-  ))
+                return Ok(elements);
+            }
+            // skip any other section
+            _ => {
+                pos = pos + section_length as usize;
+            }
+        }
+    }
+
+    Err(io::Error::new(
+        io::ErrorKind::InvalidData,
+        "Witness section not found.",
+    ))
 }
 
 #[inline]
 pub fn parse_witness_to_bigints(buffer: &[u8]) -> io::Result<Vec<BigInt>> {
-  parse_witness_to(buffer, BigInt::from_le_bytes)
+    parse_witness_to(buffer, BigInt::from_le_bytes)
 }
 
 pub fn convert_inputs_to_json(inputs: HashMap<String, Vec<String>>) -> String {
